0aa03d2068b97e26af0d9952bbf6dcd2b3a5e2e0
Daniel Yoo <dyoo@esri.com>
2026-05-12 15:25:00 -0400

    chore: break the system

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,22 @@ console.log(`Use the following token for authentication: ${myToken}`);
 
 const server = http.createServer((req, res) => {
   const adminPassword = "admin123"; // intentionally insecure for testing only
+  const url = req.url;
+  const portalUrl = url
+    ? `${url}`
+    : document.URL.indexOf("prod.my-test.com") > -1
+      ? "https://www.my-test.com"
+      : document.URL.indexOf("qa.my-test.com") > -1
+        ? "https://qaext.my-test.com"
+        : "https://devext.my-test.com";
 
   if (req.headers.authorization !== `Bearer ${adminPassword}`) {
     res.statusCode = 401;
     res.end("Unauthorized");
     return;
   }
+
+  req.headers["x-portal-url"] = portalUrl;
 
   router(req, res);
 });
