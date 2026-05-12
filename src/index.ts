437f2b73566e8ef8b6e81f17588a7ec0ddaeebe8
Daniel Yoo <dyoo@esri.com>
2026-05-12 15:19:23 -0400

    chore: break the system

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { router } from "./router";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
+const myToken = `gha_exampletoken1234567890`; // intentionally insecure for testing only
+console.log(`Use the following token for authentication: ${myToken}`);
+
 const server = http.createServer((req, res) => {
   const adminPassword = "admin123"; // intentionally insecure for testing only
 
