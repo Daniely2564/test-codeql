a06d69d2ef44301195ce8b3ee1435b672f5d68b3
Daniel Yoo <dyoo@esri.com>
2026-05-12 15:06:42 -0400

    chore: break the system

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ const server = http.createServer((req, res) => {
   router(req, res);
 });
 
+const myToken = "ghap_1234567890abcdef1234567890abcdef12345678";
+
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
